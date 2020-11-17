a9d4a966d32bc11b40e384d7356a115183c35def
Dmitry Zhiltsov <info@zhiltsov.info>
2020-11-18 01:28:09 +0300

    v1. Выполняется условие задачи, без правок вне заданной строки, но замыкания синхронные

--- main.go
+++ main.go
@@ -33,10 +33,21 @@ func (cache *InMemoryCache) Get(key Key) (Value, bool) {
 
 // GetOrSet возвращает значение ключа в случае его существования.
 // Иначе, вычисляет значение ключа при помощи valueFn, сохраняет его в кэш и возвращает это значение.
+/*
+TODO Это вариант №1 без исправления кода выше
+TODO Минусом логики является блокирующя операция на переданное замыкание. Они выполняются последовательно
+TODO Необходимо выполнение замыканий выделять в отдельный слой управления
+*/
 func (cache *InMemoryCache) GetOrSet(key Key, valueFn func() Value) Value {
-	// TODO
-}
+	if value, found := cache.Get(key); found { // если ключ в кеше - отдаем
+		return value
+	}
 
-func main() {
-
+	cache.dataMutex.Lock() // блокируем весь кеш
+	defer cache.dataMutex.Unlock()
+	if value, found := cache.data[key]; found { // еще раз проверяем ключ после блокировки всех горутин
+		return value // отдаем, если одна из горутин записала ключ, чтобы не выполнять функцию повторно
+	}
+	cache.data[key] = valueFn()
+	return cache.data[key]
 }
---
+++ main_test.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"sync"
+	"testing"
+	"time"
+)
+
+const (
+	GoCounts  = 1_000 // Количество горутин
+	CacheSize = 10     // Количество ключей результирующего кеша
+)
+
+/*
+Инкрементный счетчик записи в кеш
+*/
+type Counter struct {
+	sync.Mutex
+	v uint64
+}
+
+// +1
+func (c *Counter) Inc() {
+	c.Lock()
+	defer c.Unlock()
+	c.v++
+}
+
+var (
+	cache   = NewInMemoryCache()
+	counter = new(Counter)
+)
+
+/*
+Тест: отдай или прими!
+*/
+func TestInMemoryCache_GetOrSet(t *testing.T) {
+	var wg sync.WaitGroup
+	wg.Add(GoCounts)
+
+	fmt.Printf(
+		"Запускаем %d горутин(у,ы) [GoCounts Х CacheSize] для попытки записи в кеш по %d значений(я,ю)\n",
+		GoCounts*CacheSize,
+		CacheSize,
+	)
+	fmt.Println("Время работы каждого замыкания - 1 секунда. time.Sleep(time.Second)")
+	fmt.Printf("Ожидайте около %d секунд(а,ы)\n\n", CacheSize)
+
+	for i := 0; i < GoCounts; i++ {
+		i := i
+		go func() { // Запускаем GoCounts горутин
+			defer wg.Done()
+			var fwg sync.WaitGroup
+			fwg.Add(CacheSize)
+			for k := 0; k < CacheSize; k++ { // Читаем или пишем CacheSize значений
+				go func(i, k int) {
+					defer fwg.Done()
+					cache.GetOrSet(
+						strconv.Itoa(k), // Ключ кеша
+						func() Value { // Записываем в кеш, если ключ не найден
+							fmt.Printf("Горутина %d, ключ: %d\n", i, k)
+							time.Sleep(time.Second)    // Цена каждой записи - секунда
+							counter.Inc()              // +1 к кол-ву записей в кеш
+							return strconv.Itoa(k * k) // в результат квадрат
+						})
+				}(i, k)
+			}
+			fwg.Wait()
+		}()
+	}
+
+	wg.Wait()
+	if counter.v != CacheSize {
+		t.Errorf("Кол-во обращений на запись (%d) в кеш не совпадает с размером (%d)", counter.v, CacheSize)
+	} else {
+		fmt.Println("OK! Кол-во записи в кеш равно его размеру.")
+	}
+}
